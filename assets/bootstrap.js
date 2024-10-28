8ecd9a3fb832e0771e120a856b82f83f9eb82661
heidi-gouider <gouider.heidi@gmail.com>
2024-10-28 10:34:36 +0100

    install encore

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,9 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+// import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { Application } from '@hotwired/stimulus';
+import './styles/app.css';
 
-const app = startStimulusApp();
+// const app = startStimulusApp();
+
+const application = Application.start();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);

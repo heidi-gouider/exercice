f98468647c65b99b60d601af8481b2af58a4be4e
heidi-gouider <gouider.heidi@gmail.com>
2024-12-09 14:23:39 +0100

    modif gantt

--- react/components/diagrammeGantt.jsx
+++ react/components/diagrammeGantt.jsx
@@ -13,22 +13,27 @@ function DiagrammeGantt() {
       { type: "number", label: "Percent Complete" },
       { type: "string", label: "Dependencies" },
     ],
-    ["1", "Recherche des besoins", "Conception", new Date(2024, 0, 1), new Date(2024, 0, 2), null, 100, null],
-    ["2", "Définition des cas d'utilisation", "Conception", new Date(2024, 0, 3), new Date(2024, 0, 4), null, 100, "1"],
-    ["3", "Spécifications techniques", "Conception", new Date(2024, 0, 5), new Date(2024, 0, 7), null, 100, "2"],
-    ["4", "Développement API - Produits", "Backend", new Date(2024, 0, 8), new Date(2024, 0, 14), null, 50, "3"],
-    ["5", "Développement API - Commandes", "Backend", new Date(2024, 0, 15), new Date(2024, 0, 21), null, 20, "4"],
-    ["6", "Développement API - Clients", "Backend", new Date(2024, 0, 22), new Date(2024, 0, 28), null, 0, "5"],
-    ["7", "Développement Frontend - Catalogue", "Frontend", new Date(2024, 1, 1), new Date(2024, 1, 7), null, 0, "6"],
-    ["8", "Développement Frontend - Panier", "Frontend", new Date(2024, 1, 8), new Date(2024, 1, 14), null, 0, "7"],
-    ["9", "Développement Mobile - Catalogue", "Mobile", new Date(2024, 2, 1), new Date(2024, 2, 10), null, 0, "8"],
-    ["10", "Développement Mobile - Profil utilisateur", "Mobile", new Date(2024, 2, 11), new Date(2024, 2, 20), null, 0, "9"],
-    ["11", "Tests API", "Tests", new Date(2024, 3, 1), new Date(2024, 3, 7), null, 0, "6"],
-    ["12", "Tests Frontend et Mobile", "Tests", new Date(2024, 3, 8), new Date(2024, 3, 14), null, 0, "11"],
-    ["13", "Déploiement Backend et Frontend", "Déploiement", new Date(2024, 3, 15), new Date(2024, 3, 20), null, 0, "12"],
-    ["14", "Déploiement Mobile", "Déploiement", new Date(2024, 3, 21), new Date(2024, 3, 25), null, 0, "13"],
-    ["15", "Documentation", "Documentation", new Date(2024, 3, 26), new Date(2024, 3, 30), null, 0, "14"],
+
+    // ["id", "Nom", "Groupe", start, end, duration, percentComplete, dependencies]
+
+    ["1", "Recherche des besoins", "Conception", new Date(2024, 11, 9), new Date(2024, 11, 10), null, 100, "1"],
+    ["2", "Cahier des charges", "Conception", new Date(2024, 11, 10), new Date(2024, 11, 10), null, 100, "2"],
+    ["3", "Maquettage", "Conception", new Date(2024, 11, 10), new Date(2024, 11, 13), null, 100, "3"],
+    ["4", "Dictionnaire de données", "Conception", new Date(2024, 11, 16), new Date(2024, 11, 16), null, 100, "4"],
+    ["5", "Définition des cas d'utilisation", "Conception", new Date(2024, 11, 17), new Date(2024, 11, 17), null, 100, "5"],
+    ["6", "Spécifications techniques", "Conception", new Date(2024, 0, 5), new Date(2024, 0, 7), null, 100, "6"],
+    ["7", "Développement Frontend - Accueil", "Frontend", new Date(2024, 1, 1), new Date(2024, 1, 7), null, 0, "7"],
+    ["8", "Développement Frontend - Panier", "Frontend", new Date(2024, 1, 8), new Date(2024, 1, 14), null, 0, "8"],
+    ["9", "Tests API", "Tests", new Date(2024, 3, 1), new Date(2024, 3, 7), null, 0, "9"],
+    ["10", "Tests Frontend et Mobile", "Tests", new Date(2024, 3, 8), new Date(2024, 3, 14), null, 0, "10"],
+    ["11", "Déploiement Backend et Frontend", "Déploiement", new Date(2024, 3, 15), new Date(2024, 3, 20), null, 0, "11"],
+    ["12", "Documentation", "Documentation", new Date(2024, 3, 26), new Date(2024, 3, 30), null, 0, "12"],
   ];
+      // ["", "Développement API - Produits", "Backend", new Date(2024, 0, 8), new Date(2024, 0, 14), null, 50, "3"],
+    // ["", "Développement API - Commandes", "Backend", new Date(2024, 0, 15), new Date(2024, 0, 21), null, 20, "4"],
+    // ["", "Développement API - Clients", "Backend", new Date(2024, 0, 22), new Date(2024, 0, 28), null, 0, "5"],
+    // ["14", "Déploiement Mobile", "Déploiement", new Date(2024, 3, 21), new Date(2024, 3, 25), null, 0, "13"],
+
   const options = {
     height: 400,
     gantt: {
